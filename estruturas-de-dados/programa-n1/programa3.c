7daf632ad4867489cf8843cd01ce61f5f4015b7d
Maria Luiza <marialuiza.bf.silva@gmail.com>
2025-05-03 00:46:22 -0300

    limpeza do codigo

--- estruturas-de-dados/programa-n1/programa3.c
+++ estruturas-de-dados/programa-n1/programa3.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h> //para usar rand
-#include <time.h> //para usar time_t
+#include <stdlib.h> 
+#include <time.h> 
 #include <string.h>
 
-
-//struct para data
 typedef struct 
 {
     int dia;
@@ -12,7 +10,6 @@ typedef struct
     int ano;
 } Data; 
 
-//funçao para converter data p/ timestamp
 time_t converterData(Data data){
     struct tm t;
     t.tm_year = data.ano - 1900;
@@ -26,17 +23,13 @@ time_t geradorTimestampAleatorio(time_t periodo_dia){
     return periodo_dia + (rand() % 86400); // é o periodo do dia em segundos 
 }
 
-
-
-
-
 int main(){
-    Data data; //para usar a struct para guardar os dados quando o usuario digitar no console
+    Data data; 
     char *sensores[] = {"TEMP", "PRES", "VIBR", "UMID", "FLUX"};
     int n_sensores= 5;
     int qtd_leitura_sensores= 1000;
 
-    srand(time(NULL)); // usar srand para iniciar a funçao rand com valor semente, p/ não repetir os valores
+    srand(time(NULL)); 
 
     printf("PROGRAMA 3:\n");
     printf("Digite a data com os valores solicitados.\n");
@@ -56,7 +49,6 @@ int main(){
         return 1;
     }
 
-    //for para fazer gerar as leituras
     for (int i = 0; i < n_sensores; i++)
     {
         for (int j =0; j < qtd_leitura_sensores; j++)
@@ -68,11 +60,6 @@ int main(){
             float valor = rand() % 100; // valor aleatorio entre 0 - 100
 
             fprintf(arquivo, "%s;%ld;%.2f\n", sensores_aleatorio, timestamp, valor);
-        }
-        
+        } 
     }
-    
-
-
-
 }
